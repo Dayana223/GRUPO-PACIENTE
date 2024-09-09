87d8ae0bca0be69c976eeaf4584e2c57b9c18b70
DayanaLaime <dayanalaime91@gmail.com>
2024-09-09 18:55:46 -0300

    cambios en vista

--- vista/AgregarPacienteVista.py
+++ vista/AgregarPacienteVista.py
@@ -1,14 +1,54 @@
-from PyQt6.Qtwidgets import QWidget,QPushButton,QHBoxLayout
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit
 
 class AgregarPacienteVista(QWidget):
 
-    def __init__(self,controladorAgregarPaciente):
-        self.setGeometry(100,100,250,250)
-        self.setWindowTitle("Agregar Paciente")
-        self.__boton = QPushButton("Agregar")
-        self.__layout =QHBoxLayout()
-        self.__boton.clicked.connect(controladorAgregarPaciente)
-        self.__layout.addWidget(self.__boton)
-        self.setLayout(self.__layout) 
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle("Agregar Paciente") 
 
+        # Etiquetas y campos de texto
+        self.__dni = QLabel("DNI:")
+        self.__lineEditDni = QLineEdit()
+        
+        self.__nombre = QLabel("Nombre:")
+        self.__lineEditNombre = QLineEdit()
+        
+        self.__apellido = QLabel("Apellido:")
+        self.__lineEditApellido = QLineEdit()
+        
+        # Botón para agregar paciente
+        self.__botonGuardar= QPushButton("Guardar")
+        self.__botonCancelar = QPushButton("Cancelar")
+        # Conectar el botón al método que agregar paciente (a definir en el controlador)
+        # self.__botonAgregar.clicked.connect(controladorAgregarPaciente.agregar_paciente)
 
+        # Layout para el botón
+        self.__layoutBoton = QHBoxLayout()
+        self.__layoutBoton.addWidget(self.__botonGuardar)
+        self.__layoutBoton.addWidget(self.__botonCancelar)
+        
+        # Layout para el formulario
+        self.__layoutForm = QVBoxLayout()
+        self.__layoutForm.addWidget(self.__dni)
+        self.__layoutForm.addWidget(self.__lineEditDni)
+        self.__layoutForm.addWidget(self.__nombre)
+        self.__layoutForm.addWidget(self.__lineEditNombre)
+        self.__layoutForm.addWidget(self.__apellido)
+        self.__layoutForm.addWidget(self.__lineEditApellido)
+        
+        # Layout principal
+        self.__ventanaPrincipalLayout = QVBoxLayout()
+        self.__ventanaPrincipalLayout.addLayout(self.__layoutForm)
+        self.__ventanaPrincipalLayout.addLayout(self.__layoutBoton)
+        
+        # Setear el layout principal en la ventana
+        self.setLayout(self.__ventanaPrincipalLayout)
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ventana = AgregarPacienteVista()
+    ventana.show()  # Mostrar la ventana
+    sys.exit(app.exec())
--- vista/ListaPacientesVista.py
+++ vista/ListaPacientesVista.py
@@ -1,14 +1,45 @@
-from PyQt6.Qtwidgets import QWidget,QPushButton,QHBoxLayout
+from PyQt6.Qtwidgets import QTableWidget,QVBoxLayout,QLineEdit,QWidget,QPushButton,QHBoxLayout,QComboBox
 
 class ListarPacienteVista(QWidget):
 
-    def __init__(self,controladorListarPacienteVista):
+    def __init__(self,controladorListarPacienteVista,controladorDetallePaciente):
+        super().__init__()
         self.setGeometry(100,100,250,250)
-        self.setWindowTitle("Detalle Paciente")
-        self.__boton = QPushButton("Modificar Paciente")
-        self.__layout =QHBoxLayout()
-        self.__boton.clicked.connect(controladorListarPacienteVista)
-        self.__layout.addWidget(self.__boton)
-        self.setLayout(self.__layout) 
+        self.setWindowTitle("Pacientes ")
+
+        self.__comboBoxCriterio = QComboBox()
+        self.__comboBoxCriterio.addItems(["DNI","Apellido","Nombre"])
+        
+        self.__barraBusqueda=QLineEdit()
+        self.__barraBusqueda.setPlaceholderText("Buscar")
+        self.__barraBusqueda.textchanget.connect(self.filtrar_pacientes())
+        
+        self.__tablaPacientes =QTableWidget()# mirar
+        self.__tablaPacientes.setColumnCount(4)
+        self.__tablaPacientes.setHorizontalHeaderLabels(["Nombre","Apellido","DNI","Proximio Turno"])
+        
+        self.__botonNuevoPaciente =QPushButton("Nuevo Paciente")
+        self.__botonNuevoPaciente.clicked.connect(controladorListarPacienteVista.agregar_paciente())
+        
+        self.__botonVerPaciente =QPushButton("Ver Paciente")
+        self.__botonVerPaciente.clicked.connect(controladorDetallePaciente.verPaciente())
+        
+        self.__layoutPrincipal=QVBoxLayout()
+        self.__layoutBotones =QHBoxLayout()
+        self.__layoutBusqueda =QHBoxLayout()
+        
+        self.__layoutBusqueda.addWidget(self.__comboBoxCriterio)
+        self.__layoutBusqueda.addWidget(self.__barraBusqueda)
+        
+        self.__layoutPrincipal.addWidget(self.__layoutBusqueda)
+        self.__layoutPrincipal.addWidget(self.__tablaPacientes)
+        
+        self.__layoutBotones.addWidget(self.__botonNuevoPaciente)
+        self.__layoutBotones.addWidget(self.__botonVerPaciente)
+        
+        self.__layoutPrincipal.addLayout(self.__layoutBotones)
+        
+        self.setLayout(self.__layoutPrincipal)
+        
 
 
