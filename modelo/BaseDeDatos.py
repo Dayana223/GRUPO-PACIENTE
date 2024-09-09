d3c1cab9d07cb9911540f875ed831f3cf7b82a43
DayanaLaime <dayanalaime91@gmail.com>
2024-09-09 15:46:10 -0300

    Cambios en Modelo
    Coloque la logica de agregar paciente en el Modelo de la clase Base de datos

--- modelo/BaseDeDatos.py
+++ modelo/BaseDeDatos.py
@@ -1,13 +1,33 @@
+import psycopg2
+#from psycopg2 import Pacientes
+
 class BaseDeDatos:
     def __init__(self):
-        pass
-
+        self.__conexion =psycopg2.connect(
+            nombre ="Pacientes",
+            user = "postgres",
+            password = "2323",
+            host = "localhost",
+            port ="5432"
+            
+        )        
+        self._cursor =self.__conexion.cursor()
+        
     def obtener_paciente():
         pass
 
-    def agregar_paciente():
-        pass
-
+    def agregar_paciente(self,dni,nombre,apellido):
+        try:
+            consulta="""
+                INSERT INTO Pacientes (dni_paciente,nombre_paciente,apellido_paciente) 
+                VALUES(%s,%s,%s);
+            """
+            self.__cursor.execute(consulta,(dni,nombre,apellido))
+            self.__conexion.commit()
+            return True
+        except Exception as e:
+            self.__conexion.rollback()
+            return False
     def modificar_paciente():
         pass
 
